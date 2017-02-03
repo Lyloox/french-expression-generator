40f556e1313804f83c2dbf410b1ce3ae3def6055
Laure Daumal <daumal_l@epita.fr>
2017-02-03 17:01:30 +0100

    add files

---
+++ sample/generator.py
@@ -0,0 +1,47 @@
+import random
+import re
+
+from sounds import get_sounds
+
+def read_datas(file):
+    with open("../data/" + file + ".in") as datas:
+        data_list = [name.strip() for name in datas]
+    return data_list
+
+def random_choice(alist):
+    return alist[random.randint(0, len(alist) - 1)]
+
+def match(suffix, alist):
+    matching = []
+    for name in alist:
+        if name.endswith(tuple(suffix)):
+            matching.append(name)
+    return matching
+
+def find_matching(sounds, animals, adjectives):
+    sound = sounds[random.choice(list(sounds.keys()))]
+    #print(sound)
+
+    matching_animals = match(sound, animals)
+    matching_adjectives = match(sound, adjectives)
+    if matching_animals and matching_adjectives:
+        return matching_animals, matching_adjectives
+    else:
+        return find_matching(sounds, animals, adjectives)
+
+
+def generate():
+    f_animals = read_datas("female_animals")
+    f_adjectives = read_datas("female_adjectives")
+    m_animals = read_datas("male_animals")
+    m_adjectives = read_datas("male_adjectives")
+
+    sounds = get_sounds()
+    animals, adjectives = find_matching(sounds, f_animals, f_adjectives)
+
+    for animal in animals:
+        print(random_choice(adjectives) + " " + animal)
+
+
+generate()
+
---
+++ sample/sounds.py
@@ -0,0 +1,75 @@
+
+sounds = {
+        # 1-syll sounds sounds
+        "a": ["a"],
+        "e": ["eux"],
+        "é": ["é", "ée"],
+        "è": ["è", "ès", "ai", "aie"],
+        "i": ["i", "ie", "is", "it"],
+        "o": ["o", "au", "aux", "aud", "aut"],
+        "u": ["u", "ue"],
+        "ou": ["ou", "oux", "oue"],
+        "oi": ["oie", "oigt", "oua", "oit", "oa"],
+        "an": ["ant", "an", "en"],
+        "in": ["in", "un"],
+        "on": ["on", "om"],
+
+        # 2-syll sounds
+        "al": ["al", "alle", "ale", "âl", "âle", "âlle"],
+        "aj": ["age", "aje", "aj", "âge"],
+        "as": ["as", "asse", "ace", "âce", "âsse"],
+        "ar": ["ar", "arre", "are", "âre"],
+        "av": ["av", "ave", "âve"],
+        "ash": ["ash", "ach", "ashe", "ache", "âche"],
+        "at": ["ate", "âte"],
+        "ad": ["ade", "ad"],
+
+        "el": ["el", "elle", "èle", "aile"],
+        "euz": ["euse", "euses", "euz", "euze"],
+        "èt": ["ète", "aite"],
+        "èz": ["aise", "èse"],
+        "èr": ["aire", "ère", "ere", "erre"],
+        "èn": ["aine", "ène"],
+        "èl"
+
+        "ik": ["ique"],
+        "il": ["il", "ille", "ile"],
+        "ille": ["ille"],
+        "ine": ["ine"],
+        "ins": ["ins", "ince"],
+        "id": ["id", "ide", "ides"],
+        "iv": ["iv", "ive"],
+        "ite": ["it", "ite", "ites"],
+        "is": ["isse", "ice"],
+
+        "os": ["os", "osse", "ausse"],
+        "ob": ["ob", "obe"],
+        "oul": ["ool", "oule", "oul"],
+        "oit": ["oit", "oite"],
+        "oir": ["oir", "oire"],
+        "ouz": ["ouse", "ouz", "ouses"],
+        "ok": ["oque"],
+
+        "ut": ["ute"],
+        "ur": ["ur", "ure", "ûre"],
+
+        # 3-syll sounds
+        "able": ["able"],
+        "ant": ["ante", "ente"],
+        "and": ["ande"],
+        "alm": ["alm", "alme"],
+        "ansh": ["ansh", "anch", "anshe", "anche"],
+
+        "ègr": ["ègre", "aigre"],
+
+        "ist": ["ist", "iste", "ïste", "ïst"],
+        "ible": ["ible"],
+
+        "opr": ["opre"],
+        "ourb": ["ourb", "ourbe"],
+        "onbr": ["ombre", "onbre"]
+
+        }
+
+def get_sounds():
+    return sounds
