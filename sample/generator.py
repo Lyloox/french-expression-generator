75e4cd3249d0c840286e273489994ac546e64b7a
Laure Daumal <daumal_l@epita.fr>
2017-02-04 13:14:46 +0100

    add sentence generation

--- sample/generator.py
+++ sample/generator.py
@@ -56,45 +56,69 @@ def generate_animals(options):
                     print(adjective + " " + random_choice(animals))
                 i += 1
 
+def generate_sentence(*words):
+    if len(words) == 1 and type(words[0]) is list:
+        # Allow multiple arguments or a list in one argument
+        words = words[0]
+    return ' '.join(words)
 
+def generate_random_sentence(*lists):
+    chosen = []
+    for alist in lists:
+        chosen.append(random_choice(alist))
+    return generate_sentence(chosen)
 
 def generate_expressions(options):
     verbs = read_datas("verbs")
     adverbial_phrases = read_datas("adverbial_phrases")
     coordinators = read_datas("coordinators")
     prepositions = read_datas("prepositions")
-    m_names = read_datas("male_names")
-    f_names = read_datas("female_names")
+    m_s_names = read_datas("male_singular_names")
+    f_s_names = read_datas("female_singular_names")
+    m_p_names = read_datas("male_plural_names")
+    f_p_names = read_datas("female_plural_names")
+
+    f_adjectives = read_datas("female_adjectives")
+    m_adjectives = read_datas("male_adjectives")
+
+    names = m_s_names + m_p_names + f_s_names + f_p_names
 
     i = 0
     while (i < options.n):
-        x = random.randint(0, 3)
+        x = random.randint(0, 1)
         if options.debug:
             print(x)
         if x == 0:
-            print(random_choice(verbs) + " " + \
-                            random_choice(m_names + f_names) + " " + \
-                            random_choice(adverbial_phrases)
-                    )
+            print(generate_random_sentence(verbs, names, adverbial_phrases))
         elif x == 1:
-            print(random_choice(verbs) + " " + \
-                            random_choice(m_names + f_names) + " " + \
-                            random_choice(prepositions) + " " + \
-                            random_choice(m_names + f_names) + " "
-                    )
+            print(generate_random_sentence(verbs, names, prepositions, names))
+        '''
         elif x == 2:
+            female = bool(random.getrandbits(1))
+            plural = bool(random.getrandbits(1))
+
+            if plural:
+                adj = random_choice(f_adjectives if female else m_adjectives)
+                if not adj.endswith("x"):
+                    adj = adj + "s"
+                print(adj)
+
+
             print(random_choice(prepositions) + " " + \
-                            random_choice(m_names + f_names) + " " + \
-                            random_choice(prepositions) + " " + \
-                            random_choice(m_names + f_names) + " " + \
-                            random_choice(coordinators) + " " + \
-                            random_choice(m_names + f_names)
+                    random_choice(names) + " " + \
+                    random_choice(prepositions) + " " + \
+                    random_choice(names) + " " + \
+                    random_choice(coordinators) + " " + \
+
+                    random_choice(f_p_names if female else m_p_names) + \
+                    " est " + random_choice(f_adjectives if female
+                        else m_adjectives)
+                    if plural else
+                    random_choice(f_s_names if female else m_s_names) + \
+                    " sont " + adj
                     )
         elif x == 3:
-            print(random_choice(verbs) + " " + \
-                            random_choice(prepositions) + " " + \
-                            random_choice(m_names + f_names) + " " + \
-                            random_choice(coordinators) + " " + \
-                            random_choice(m_names + f_names) + " "
-                    )
+            print(generate_random_sentence(verbs, prepositions, names,
+                coordinators, name))
+        '''
         i += 1
