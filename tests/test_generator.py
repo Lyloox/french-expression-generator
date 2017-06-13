fc16d3fe126ed0151fc02b88803194fc14af6970
Laure Daumal <daumal_l@epita.fr>
2017-06-13 23:30:57 +0200

    add more tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,9 +1,11 @@
 #!/user/bin/env python
-import unittest
 import sys
 sys.path.insert(0, "generator")
 import generator
+
+import unittest
 import random
+from functools import partial
 
 class Options:
     debug = None
@@ -19,17 +21,27 @@ class TestGenerator(unittest.TestCase):
 
     random.seed(42)
 
-    def test_length(self):
-        """
-        Test length(s) of generated list(s)
-        """
-        n_tests = 50
+    def assert_length(self, func, n_tests=50, name=None):
         options = Options()
         for i in [random.randrange(10, 1000) for _ in range(n_tests)]:
             options.n = i
-            print("Generating " + str(i) + " samples...")
-            liste = generator.generate_animals(options)
-            self.assertEqual(len(liste), options.n)
+            print("Generating " + str(i) + " samples", end="")
+            print((" for %s test..." % name) if name else "...")
+            self.assertEqual(len(func(options)), options.n)
+
+    def test_length_animals(self):
+        """
+        Test lengths of generated animals lists
+        """
+        self.assert_length(partial(generator.generate_animals),
+                name="animals")
+
+    def test_length_expressions(self):
+        """
+        Test lengths of generated expressions lists
+        """
+        self.assert_length(partial(generator.generate_expressions),
+                name="expressions")
 
     if __name__ == "__main__":
         unittest.main()
